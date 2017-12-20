5819aa73078d0f3b7d215c24b80aa783d5a1f9f1
Petter Ahlberg <petter.ahlberg@tui.se>
2017-12-20 14:06:57 +0100

    Add basic key/value -> label/input feature

--- index.js
+++ index.js
@@ -4,7 +4,21 @@
 "use strict";
 
 module.exports = {
-    getForm: function () {
-        return "<form></form>";
+    getForm: function (json) {
+        let html = "";
+
+        if (json) {
+            Object.keys(json).forEach(function (key) {
+                html += "<label>";
+                html += "<span>" + key + "</span>";
+                html += "<input type='text' value='" + json[key] + "'>";
+                html += "</label>";
+            });
+        }
+
+        let rootFs = json ? "<fieldset><legend>Root</legend>" + html + "</fieldset>" : "";
+        let formHtml = "<form>" + rootFs + "</form>";
+
+        return formHtml;
     }
 };
--- test/test.js
+++ test/test.js
@@ -2,16 +2,21 @@
     es6: true, node: true
 */
 /*global
-    describe, it
+    describe, it, before, document
 */
 "use strict";
 
 const chai = require("chai");
 const expect = chai.expect;
+const jsdom = require("mocha-jsdom");
 
 const jthf = require("../index");
 
+let $;
+jsdom();
+
 describe("The json-to-html-form module", function () {
+
     it("should exist", function () {
         expect(jthf).to.be.an("object");
     });
@@ -21,10 +26,49 @@ describe("The json-to-html-form module", function () {
     });
 
     describe("The 'getForm' method", function () {
-        it("should return an empty form (html string) if no argument(s) were supplied", function () {
-            let html = jthf.getForm();
+        before(function () {
+            $ = require('jquery');
+        });
 
-            expect(html).to.equals("<form></form>");
+        describe("The HTML (return value)", function () {
+
+            it("should contain an empty form (html string) if no argument(s) were supplied", function () {
+                let html = jthf.getForm();
+
+                expect(html).to.equals("<form></form>");
+            });
+
+            it("should contain a <form> contaning and a 'root' fieldset if the (json, object) argument is '{}' (an object)", function () {
+                let html = jthf.getForm({});
+
+                expect(html).to.equals("<form><fieldset><legend>Root</legend></fieldset></form>");
+            });
+
+            it("should add a label with a span and an input[type='text'] to all key/value pairs (where the value is a string)", function () {
+                let testJson = {
+                    "string1": "one",
+                    "string2": "two"
+                };
+                let html = jthf.getForm(testJson);
+
+                document.body.innerHTML = html;
+
+                let labels = $("fieldset label");
+
+                let label1 = labels.eq(0);
+                let span1 = label1.find("span");
+                let inp1 = label1.find("input[type='text']");
+
+                expect(span1.text()).to.equals("string1");
+                expect(inp1.val()).to.equals("one");
+
+                let label2 = labels.eq(1);
+                let span2 = label2.find("span");
+                let inp2 = label2.find("input[type='text']");
+
+                expect(span2.text()).to.equals("string2");
+                expect(inp2.val()).to.equals("two");
+            });
         });
     });
 });
